69d0993d3838789953a6f2722e762e5b1388b1f3
Fisboll <>
2023-05-29 18:34:50 +0000

    Added Hiroku as allowed host

--- gamingnews/settings.py
+++ gamingnews/settings.py
@@ -19,7 +19,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = ['fullstack-fisboll.herokuapp.com', 'localhost',
     '8000-fisboll-fullstack-6pg18hwmw5.us2.codeanyapp.com']
 
 
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'gamingnews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
