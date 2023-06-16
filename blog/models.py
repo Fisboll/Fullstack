580d700040b7cf73f452a49b2783d324be4c5778
Fisboll <>
2023-06-16 10:06:57 +0000

    Line to long issue

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,8 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, 'Draft'), (1, 'Published'))
 SUBGENRES = (
-    (0, 'Strategy'), (1, 'Survival and horror'), (2, 'Fantasy'), (3, 'Sci-Fi'), (
-        4, 'Action-adventure'), (5, 'Comedy'), (7, 'Cyberpunk'), (
-            8, 'Apocalyptic'), (9, 'Other'))
+    (0, 'Strategy'), (1, 'Survival and horror'), (2, 'Fantasy'), (3, 'Sci-Fi'),
+    (4, 'Action-adventure'), (5, 'Comedy'), (7, 'Cyberpunk'),
+    (8, 'Apocalyptic'), (9, 'Other'))
 
 # Create your models here.
