2826c80b7992947fbed7dfc151294d3d622e6e1b
Fisboll <>
2023-06-16 10:05:18 +0000

    Added status adn subgenres for models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auht.models import User
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, 'Draft'), (1, 'Published'))
+SUBGENRES = (
+    (0, 'Strategy'), (1, 'Survival and horror'), (2, 'Fantasy'), (3, 'Sci-Fi'), (
+        4, 'Action-adventure'), (5, 'Comedy'), (7, 'Cyberpunk'), (
+            8, 'Apocalyptic'), (9, 'Other'))
 
 # Create your models here.
