b6c07c2cd710a329479b2601ae0f4296a6640f3d
Fisboll <>
2023-06-21 18:48:47 +0000

    Added more modules and fixed the admin panel for super user.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Game, Comment, User_profile
 from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Game)
+class GameAdmin(SummernoteModelAdmin):
+
+    Summernote_fields = ('content')
 
 
 @admin.register(Post)
@@ -8,3 +14,14 @@ class PostAdmin(SummernoteModelAdmin):
 
     Summernote_fields = ('content')
 
+
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
+
+    Summernote_fields = ('content')
+
+
+@admin.register(User_profile)
+class User_profileAdmin(SummernoteModelAdmin):
+
+    Summernote_fields = ('content')
--- blog/models.py
+++ blog/models.py
@@ -9,23 +9,26 @@ SUBGENRES = (
     (8, 'Apocalyptic'), (9, 'MMORPG'))
 
 
-class game(models.Model):
+class Game(models.Model):
     name = models.CharField(max_length=50)
     platform = models.CharField(max_length=100)
     image = CloudinaryField('image', default='placeholder')
     description = models.TextField(max_length=2000)
     release_date = models.DateTimeField(setattr)
 
+    def __str__(self):
+        return self.name.__str__()
+
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
     subTitle = models.CharField(max_length=100)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE,
-                                   related_name='game_posts')
+                                   related_name='post_user')
     content = models.TextField(max_length=2000)
     image = CloudinaryField('image', default='placeholder')
-    comments = models.BigIntegerField()
-    game = models.BigIntegerField()
+    game = models.ForeignKey(Game, on_delete=models.CASCADE,
+                             related_name='game_posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
@@ -42,19 +45,30 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(User, on_delete=models.CASCADE,
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name='comments')
     name = models.CharField(max_length=60)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE,
+                                   related_name='comment_user')
     email = models.EmailField()
 
     class Meta:
         ordering = ['created_on']
 
     def __str__(self):
-        return f"comment {self.content} by {self.name}"
+        return f"comment {self.name} by {self.created_by}"
 
+
+class User_profile(models.Model):
+    avatar = CloudinaryField('image', default='placeholder')
+    display_name = models.TextField(max_length=30)
+    first_name = models.TextField(max_length=30)
+    last_name = models.TextField(max_length=30)
+    location = models.TextField(max_length=50)
+    is_mod = models.BooleanField(default=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,
+                              related_name='user_profile')
 
 # Create your models here.
