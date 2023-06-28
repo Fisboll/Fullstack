25dd8cfbe413cab18659cd93de210997b26e743f
Fisboll <>
2023-06-28 17:32:42 +0000

    Added another template and set up its view and url.py sections.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from django.utils.translation import ugettext_lazy as _
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home')
+    path('', views.HomePage.as_view(), name='home'),
+    path('playstation/', views.Playstation.as_view(), name='playstation')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
+from django.views.generic import CreateView, UpdateView, DeleteView, TemplateView, ListView
+from django.http import HttpResponseRedirect
 from django.views import generic
 from .models import Post
 
 
+class HomePage (TemplateView):
+    """
+    Renders home page
+    """
+    template_name = 'index.html'
+
+
+class Playstation (TemplateView):
+    """
+    Render Playstation page
+    """
+    template_name = 'playstation.html'
+
+
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    queryset = Post.objects.filter(status=1).order_by("-created_at")
     template_name = "index.html"
     paginate_by = 6
