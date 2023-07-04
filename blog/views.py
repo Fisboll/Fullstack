c9de397819916906f70cb2c03523a0c124ef65e5
Fisboll <>
2023-07-04 17:29:55 +0000

    added Post Details

--- blog/views.py
+++ blog/views.py
@@ -19,6 +19,7 @@ class Playstation (generic.ListView):
     """
     model = Game
     queryset = Game.objects.filter(platform='Playstation')
+    queryset = Game.objects.filter(platform='Xbox')
     template_name = 'playstation.html'
 
 
@@ -27,3 +28,24 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_at")
     template_name = "index.html"
     paginate_by = 6
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comment.filter(approved=True).order_by('created_at')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exist():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            }
+        )
