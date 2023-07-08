e9962b00744d3283e17fc8168504fc12b688a5db
Fisboll <>
2023-07-08 09:32:33 +0000

    changed debug to false and removed the allowed hosts for test page.

--- gamingnews/settings.py
+++ gamingnews/settings.py
@@ -29,12 +29,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['fullstack-fisboll.herokuapp.com', 'localhost',
-                 '8000-fisboll-fullstack-6pg18hwmw5.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['fullstack-fisboll.herokuapp.com', 'localhost']
 
 
 # Application definition
